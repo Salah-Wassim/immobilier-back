fed3373a0a250155963c572eda4b229ba69b9c9c
Salah-Wassim <salahwassim.arfae1@gmail.com>
2025-05-09 10:49:06 +0200

    Removal of auth-admin middleware from the login-admin and add-realtor routes. auth-realtor middleware added to the edit-realtor route. Addition of an identification check for modifying a realtor

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -7,7 +7,7 @@ const admin_controller = require('../controller/admin.controller');
 router.get('/', authAdmin(), admin_controller.find_all_admin);
 router.get('/:id', authAdmin(), admin_controller.find_one_admin);
 router.post('/new-admin', authAdmin(), admin_controller.create_admin);
-router.post('/login-admin', authAdmin(), admin_controller.login_admin);
+router.post('/login-admin', admin_controller.login_admin);
 router.put('/edit-admin/:id', authAdmin(), admin_controller.edit_admin);
 router.delete('/delete-admin/:id', authAdmin(), admin_controller.delete_admin);
 
